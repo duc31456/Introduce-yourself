ba235bc8f4ee44eea9e7f0cec3f65a328057e616
duc31456 <desomqua@gmail.com>
2022-03-28 12:16:07 +0700

    push

---
+++ 1.js
@@ -0,0 +1,11 @@
+﻿ $(function(){
+ 	// $(".mo .row .fixmenu .menu li i").hover(function() {
+ 	// 	$(this).prev().addClass('hoverra');
+ 	// 	$(".mo .row .fixmenu .menu li").addClass('hoverlira');
+ 	// }, function() {
+ 		
+
+ 	// 	// $(".mo .row .fixmenu .menu li p").removeClass('hoverra');
+ 	// });
+})  
+ 
